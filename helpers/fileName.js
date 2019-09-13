346053356e1c1eb84d8b8efda138f4049b8e6113
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-13 21:24:07 +0300

    save file in db

--- helpers/fileName.js
+++ helpers/fileName.js
@@ -1,3 +1,11 @@
+const Upload = require("../models/Upload");
+
 module.exports = (req, file, cb) => {
-  cb(null, `${Date.now()}-${file.originalname}`);
+  const name = `${Date.now()}-${file.originalname}`;
+  const extension = file.originalname.split(".")[1];
+  const upload = new Upload({ name, extension });
+
+  upload.save().catch(O_O => console.log);
+
+  cb(null, name);
 };
