152afa3de1533478f1dc03d19c9445040b95ec9a
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-13 21:22:22 +0300

    converters

---
+++ helpers/time.js
@@ -0,0 +1,11 @@
+const MS = 1000;
+const MINUTE = MS * 60; // 60 SECONDS = 1 MINUTE
+const HOUR = MINUTE * 60; // 60 MINUTES = 1 HOUR
+const DAY = MINUTE * 24; // 24 HOURS = 1 DAY
+
+module.exports = {
+  MS,
+  MINUTE,
+  HOUR,
+  DAY
+};
