6d9e562010e0eed7b3ca3aacb9b771f831f5c361
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-13 22:44:40 +0300

    fixed issues

--- helpers/fileName.js
+++ helpers/fileName.js
@@ -1,9 +1,13 @@
 const Upload = require("../models/Upload");
 const getUrl = require("./getUrl");
+const getExtension = require("./getExtension");
+const clearName = require("./clearName");
 
 module.exports = ({ protocol, hostname, ...req }, file, cb) => {
-  const name = `${Date.now()}-${file.originalname}`;
-  const extension = file.originalname.split(".")[1];
+  const originalName = file.originalname;
+  const clearedName = clearName(originalName);
+  const name = `${Date.now()}-${clearedName}`;
+  const extension = getExtension(clearedName);
   const url = getUrl({ protocol, hostname, fileName: name });
   const upload = new Upload({ name, extension, url });
 
