86c571bd556f40c07dd2dac39653486739c37a1b
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-13 22:11:32 +0300

    get all uploads endpoint

--- routes/api/uploads/index.js
+++ routes/api/uploads/index.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 
+const get = require("./actions/get");
 const store = require("./actions/store");
 const _delete = require("./actions/delete");
 
@@ -8,6 +9,9 @@ const multer = require("../../../middleware/multer");
 
 const router = Router();
 
-router.post("/", multer.array("files", 5), store);
+router
+  .get("/", get)
+  .post("/", multer.array("files", 5), store)
+  .delete("/:id", _delete);
 
 module.exports = router;
