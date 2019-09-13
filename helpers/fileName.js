485b1b2e7ab9d305d2a518e415872c3afa45fc63
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-13 22:09:35 +0300

    calling getUrl helper

--- helpers/fileName.js
+++ helpers/fileName.js
@@ -1,9 +1,11 @@
 const Upload = require("../models/Upload");
+const getUrl = require("./getUrl");
 
-module.exports = (req, file, cb) => {
+module.exports = ({ protocol, hostname, ...req }, file, cb) => {
   const name = `${Date.now()}-${file.originalname}`;
   const extension = file.originalname.split(".")[1];
-  const upload = new Upload({ name, extension });
+  const url = getUrl({ protocol, hostname, fileName: name });
+  const upload = new Upload({ name, extension, url });
 
   upload.save().catch(O_O => console.log);
 
