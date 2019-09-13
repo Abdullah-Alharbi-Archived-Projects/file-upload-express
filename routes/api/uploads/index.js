b1ca4c804e95b2051dfa92dff4e5c1dcbcf82c9f
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-13 21:24:56 +0300

    uploads entry

---
+++ routes/api/uploads/index.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const store = require("./actions/store");
+const _delete = require("./actions/delete");
+
+// middlewares
+const multer = require("../../../middleware/multer");
+
+const router = Router();
+
+router.post("/", multer.array("files", 5), store).delete("/:id", _delete);
+
+module.exports = router;
